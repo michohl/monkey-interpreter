53003f40bfa1edf89f22c79163007c5ce596e1e4
Michael Riesberg-Timmer <MichaelRT@pallid.dev>
2024-07-28 20:43:52 -0500

    feat: Add support for strings as a data type

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -299,6 +299,20 @@ ourFunction(20) + first + second;`
 	testIntegerObject(t, testEval(input), 70)
 }
 
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
